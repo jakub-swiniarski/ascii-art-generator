fbeeb1b9efaa137769131f97b2cc22fa7dd0617a
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-07-10 14:25:57 +0200

    2dim array

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ img=Image.open(path);
 
 pixels=img.load();
 width, height = img.size;
+pix = [[0 for x in range(height)] for y in range(width)]; 
 
 print(pixels[1,1]);
+
+for i in range(width):
+    for j in range(height):
+        pix[i][j]=pixels[i,j];
+
+print(pix[1][1]);
