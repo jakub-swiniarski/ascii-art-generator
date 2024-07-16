a2766292cda3cdeb8b7c970cb34783db2f9fb2bd
Jakub Świniarski <77209709+jakub-swiniarski@users.noreply.github.com>
2024-07-16 09:39:55 +0200

    main function

--- main.py
+++ main.py
@@ -1,63 +1,67 @@
 from PIL import Image;
 
-path          = str(input("Input the image path: "));
-width_desired = int(input("Input the desired image width (characters): "))
-img           = Image.open(path);
-width, height = img.size;
-ratio         = width / height;
-img           = img.resize((width_desired, int(width_desired / ratio / 2)), Image.LANCZOS); # divide by 2 because characters are not squares
-width, height = img.size;
-pixels        = img.load();
+def main():
+    path          = str(input("Input the image path: "));
+    width_desired = int(input("Input the desired image width (characters): "))
+    img           = Image.open(path);
+    width, height = img.size;
+    ratio         = width / height;
+    img           = img.resize((width_desired, int(width_desired / ratio / 2)), Image.LANCZOS); # divide by 2 because characters are not squares
+    width, height = img.size;
+    pixels        = img.load();
 
-img.close();
+    img.close();
 
-f=open("output.txt", "w");
+    f=open("output.txt", "w");
 
-for y in range(height):
-    for x in range(width):
-        (r, g, b) = pixels[x, y];
-        sum = r + g + b;
-        if sum > 600:
-            char = "'";
+    for y in range(height):
+        for x in range(width):
+            (r, g, b) = pixels[x, y];
+            sum = r + g + b;
+            if sum > 600:
+                char = "'";
 
-        elif sum > 550:
-            char = '"';
+            elif sum > 550:
+                char = '"';
 
-        elif sum > 500:
-            char = ":";
+            elif sum > 500:
+                char = ":";
 
-        elif sum > 450:
-            char = ";"
+            elif sum > 450:
+                char = ";"
 
-        elif sum > 400:
-            char = "!";
-    
-        elif sum > 350:
-            char = "+";
-
-        elif sum > 300:
-            char = "?";
- 
-        elif sum > 250:
-            char = "/"
-
-        elif sum > 200:
-            char = "%";
- 
-        elif sum > 150:
-            char = "#";
-
-        elif sum > 100:
-            char = "&";
-            
-        elif sum > 50:
-            char = "$";
-
-        else:
-            char = "@";
+            elif sum > 400:
+                char = "!";
         
-        f.write(char);
-    f.write("\n");
+            elif sum > 350:
+                char = "+";
 
-f.close();
-print("The output is saved in 'output.txt'.");
+            elif sum > 300:
+                char = "?";
+     
+            elif sum > 250:
+                char = "/"
+
+            elif sum > 200:
+                char = "%";
+     
+            elif sum > 150:
+                char = "#";
+
+            elif sum > 100:
+                char = "&";
+                
+            elif sum > 50:
+                char = "$";
+
+            else:
+                char = "@";
+            
+            f.write(char);
+        f.write("\n");
+
+    f.close();
+    print("The output is saved in 'output.txt'.");
+
+if __name__ == "__main__":
+    main()
